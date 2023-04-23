e0f1e56325394707caa4a8e555b35dd32a3633a7
AkashMaji2 <20352361-AkashMaji2@users.noreply.replit.com>
2023-04-23 07:32:03 +0000

    Initial commit

---
+++ learn-to-code-with-python-incomplete/learn-to-code-with-python-incomplete/02-Welcome-to-Python/objects-strings.py
@@ -0,0 +1,49 @@
+# strings in python
+# in python string is important data structue (object)
+
+"This is an important string"
+'This is also an important string'
+'''Woaaa this is also a string'''
+"""Wohoooo again a very funny string"""
+
+
+"He said to me: I'm in a meeting"
+'He told me that "ramu" is a gangster'
+"""This is "really" a very 'funny' string"""
+
+# strings can be in double / single or triple double / triple single quotes
+
+# empty string
+""
+''
+""""""
+''''''
+
+# space is also a string
+" "
+' '
+''' '''
+""" """
+"                "
+
+# string can contain any printable/visible character
+"This is simple string"
+"A quick brown frox jumps over the lazy dog"
+"The teacher said '2+3 is 5 not 6 !'"
+"The total bills is $5.00 and I am having only $4.99 :("
+
+# what string should I choose?
+# depends on the project/team preference
+# depends on personal choice
+
+# why do we have triple double/single string in python
+# they are meant to be called docstring
+# used for writing bigger textual paragraphs or documentation
+# they preserverse spaces, newlines and tabs as we insert
+
+"""
+A geat quote was brought to me by my teacher:
+     Democracy is by the people, of the people and for the people.
+                                                 - Abraham Lincoln
+"""
+
