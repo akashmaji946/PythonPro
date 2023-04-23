009588815c0be613845a75f6afed33fbc1fbd03b
akashmaji946 <49202839+akashmaji946@users.noreply.github.com>
2023-04-23 08:09:29 +0000

    print() function intro

---
+++ PythonPro/PythonProTopics/02-Welcome-to-Python/the-print-function-I-single-argument.py
@@ -0,0 +1,18 @@
+# Topic: print() function in python
+
+# what is a function?
+# it is a sequence of steps or a series of instructions bundled as a block
+# we can invoke / call it as many times as need
+# it is write once use many times type of logic
+
+# there are two kinds of functions in python
+# user defined and bult in
+
+# print() sum() eval() etc are built in
+
+print("This is a line in python")
+print("We're learning python")
+print(8 * 8 - 6 / 2)
+print("2 * 4 - 8")
+print("34" + "6")
+print("1" * 5)
